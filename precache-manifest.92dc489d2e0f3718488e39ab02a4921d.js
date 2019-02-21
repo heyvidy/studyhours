2c24bb3239c050a23a5494d24fd6d6f1a73a90e6
Vidyadhar <justvidyadhar@gmail.com>
2019-02-21 15:51:04 +0530

    Include manifest

---
+++ precache-manifest.92dc489d2e0f3718488e39ab02a4921d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "670dea440d5ce5745441",
+    "url": "/static/js/main.670dea44.chunk.js"
+  },
+  {
+    "revision": "1f5ddbef3cb88c00ee75",
+    "url": "/static/js/2.1f5ddbef.chunk.js"
+  },
+  {
+    "revision": "670dea440d5ce5745441",
+    "url": "/static/css/main.88baffd2.chunk.css"
+  },
+  {
+    "revision": "67d85b7c36f673645308e6f52fc1889d",
+    "url": "/index.html"
+  }
+];
